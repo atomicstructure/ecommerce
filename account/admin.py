1cbdef9d2d5d2b7201e5199315a72c47c313f095
Ogundare, Olusesi Oluwafemi <ogundarearnold@gmail.com>
2024-04-25 16:44:29 +0100

    Made the Account Admin Password a Read Only field and also some fields were made read only fields

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . models import Account
 # Register your models here.
 
+class AccountAdmin(UserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'username', 'phone_number', 'created_at','last_login', 'is_admin', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name', 'username')
+    list_display_links = ('email', 'first_name', 'last_name', 'username')
+    readonly_fields = ('created_at', 'last_login', 'password')
+    ordering = ('-created_at',)
+    
 
-admin.site.register(Account)
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(Account, AccountAdmin)
