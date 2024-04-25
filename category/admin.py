f4f854cf3a830bf0a4cfc312568dbe77c0701c59
Ogundare, Olusesi Oluwafemi <ogundarearnold@gmail.com>
2024-04-25 17:14:45 +0100

    I made the slug field a read only field and automaticaly take from the Category Name field

--- category/admin.py
+++ category/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from . models import Category
 # Register your models here.
 
-admin.site.register(Category)
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('Category_Name',)}
+    list_display = ('Category_Name', 'slug')
+
+
+
+
+admin.site.register(Category, CategoryAdmin)
