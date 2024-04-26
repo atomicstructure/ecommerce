633e0d4151d51ba6b45798069db94a13a5d0eb9f
Ogundare, Olusesi Oluwafemi <ogundarearnold@gmail.com>
2024-04-26 15:32:49 +0100

    Added function to define category slug

--- store/views.py
+++ store/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from category.models import Category
 from store.models import Products
 # Create your views here.
 
-def store(request):
-    products = Products.objects.all().filter(is_available=True)
-    product_count = products.count()
+def store(request, category_slug=None):
+    categories = None
+    products = None
+
+    if category_slug != None:
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Products.objects.filter(category=categories, is_available=True)
+        product_count = products.count()
+    else:
+        products = Products.objects.all().filter(is_available=True)
+        product_count = products.count()
+    
     context = {
         'products': products,
         'product_count': product_count
