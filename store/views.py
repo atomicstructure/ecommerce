d34643a235fcc36be769db3ddfe96c0225ad4abf
Ogundare, Olusesi Oluwafemi <ogundarearnold@gmail.com>
2024-04-30 13:01:46 +0100

    Corrected the filter order

--- store/views.py
+++ store/views.py
@@ -18,7 +18,7 @@ def store(request, category_slug=None):
         paged_products = paginator.get_page(page)
         product_count = products.count()
     else:
-        products = Products.objects.all().filter(is_available=True)
+        products = Products.objects.all().filter(is_available=True).order_by('id')
         # Pagination
         paginator = Paginator(products, 6)
         page = request.GET.get('page')
