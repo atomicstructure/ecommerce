73df963bbf547e721a2ad00208a9eb60d6eaccd9
Ogundare, Olusesi Oluwafemi <ogundarearnold@gmail.com>
2024-05-03 15:48:33 +0100

    Adding the registration page and logics

---
+++ account/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from .models import Account
+
+
+class AccountForm(forms.ModelForm):
+    class Meta:
+        model = Account
+        fields = ['first_name', 'last_name', 'email', 'phone_number', 'password']
+        widgets = {
+            'password': forms.PasswordInput()
+        }
--- account/views.py
+++ account/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django_countries import countries
 
+from account.forms import AccountForm
 # Create your views here.
+
+
+def register(request):
+    form = AccountForm()
+    context = {
+        "countries": countries,
+        "form": form
+        
+    }
+    return render(request, "account/register.html", context)
+
+
+def login(request):
+    return render(request, "account/signin.html")
+
+
+def logout(request):
+    return
+
+def forgot_password(request):
+    return
