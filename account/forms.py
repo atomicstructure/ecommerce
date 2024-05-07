ae3518e347e382e1531fbea2033573449ab11aa3
Ogundare, Olusesi Oluwafemi <ogundarearnold@gmail.com>
2024-05-07 23:20:48 +0100

    Modified register forms

--- account/forms.py
+++ account/forms.py
@@ -16,16 +16,4 @@ class AccountForm(forms.ModelForm):
         model = Account
         fields = ['first_name', 'last_name', 'email', 'phone_number', 'password', 'confirm_password',]
     
-    def __init___(self, *args, **kwargs):
-        super(AccountForm, self).__init__(*args, **kwargs)
-        
-        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter First Name'
-        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter Last Name'
-        self.fields['email'].widget.attrs['placeholder'] = 'Enter Email'
-        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter Phone Number'
-        self.fields['password'].widget.attrs['placeholder'] = 'Enter Password'
-        self.fields['confirm_password'].widget.attrs['placeholder'] = 'Confirm Password'
-        
-        for field in self.fields:
-            self.fields[field].widget.attrs['class'] = 'form-control'
-            
+    
--- carts/views.py
+++ carts/views.py
@@ -75,6 +75,8 @@ def add_cart(request, product_id):
     return redirect('cart')
 
 def cart(request, total=0, quantity=0, cart_items=None):
+    tax = 0
+    grand_total = 0
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
