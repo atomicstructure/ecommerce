4506b2f31ea117e1c3a63125568dba5db869d98f
Ogundare, Olusesi Oluwafemi <ogundarearnold@gmail.com>
2024-05-10 09:07:05 +0100

    Worked on the checkout views, urls and the checkout.html

--- carts/urls.py
+++ carts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('remove_cart/<int:product_id>/<int:cart_item_id>/', views.remove_cart, name='remove_cart'),
     path('delete_cart_item/<int:product_id>/<int:cart_item_id>/', views.delete_cart_item, name='delete_cart_item'),
     path('cart/add_cart/<int:product_id>/', views.add_cart, name='add_cart'),
+    path('checkout/', views.checkout, name='checkout'),
 ]
--- carts/views.py
+++ carts/views.py
@@ -121,3 +121,27 @@ def delete_cart_item(request, product_id, cart_item_id):
     except:
         pass
     return redirect('cart')
+
+
+def checkout(request, total=0, quantity=0, cart_items=None):
+    tax = 0
+    grand_total = 0
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+        tax = (2 * total) / 100
+        grand_total = total + tax
+    except ObjectDoesNotExist:
+        pass
+    context = {
+        'cart_items': cart_items, 
+        'total': total, 
+        'quantity': quantity,
+        'tax': tax,
+        'grand_total': grand_total,
+    }
+    
+    return render(request, 'store/checkout.html', context)
