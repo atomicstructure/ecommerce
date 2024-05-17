e865260765e0080330a5181eba31fe9be69c16c9
Ogundare, Olusesi Oluwafemi <ogundarearnold@gmail.com>
2024-05-17 08:56:40 +0100

    Modified Cart counter and Cart View to handle Logged in users

--- carts/context_processors.py
+++ carts/context_processors.py
@@ -2,24 +2,22 @@ from carts.views import _cart_id
 from .models import Cart, CartItem
 
 def cart(request):
+    cart_count = 0
     if 'admin' in request.path:
         return {}
     else:
         try:
             cart = Cart.objects.filter(cart_id=_cart_id(request))
-            cart_items = CartItem.objects.all().filter(cart=cart[:1])
-            total = 0
-            count = 0
+            if request.user.is_authenticated:
+                cart_items = CartItem.objects.all().filter(user=request.user)
+            else:
+                cart_items = CartItem.objects.all().filter(cart=cart[:1])
+
             for cart_item in cart_items:
-                total += (cart_item.product.price * cart_item.quantity)
-                count += cart_item.quantity
-        except Exception as e:
-            total = 0
-            count = 0
-            cart_items = 0
+                cart_count += cart_item.quantity
+        except Cart.DoesNotExist:
+            cart_count = 0
         context = {
-            'total': total,
-            'count': count,
-            'cart_items': cart_items
+            'cart_count': cart_count
         }
         return context
