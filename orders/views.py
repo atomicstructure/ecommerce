052bfdab2a6f023f074c51bd5eb0c69e2e916469
Ogundare, Olusesi Oluwafemi <ogundarearnold@gmail.com>
2024-05-21 02:36:44 +0100

    Added Models for Payment, Orders and OrderProduct

---
+++ orders/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def order_list(request):
+    return render(request, 'orders/order_list.html')
--- samcart/settings.py
+++ samcart/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'account',
     'store',
     'carts',
+    'orders',
 ]
 
 MIDDLEWARE = [
